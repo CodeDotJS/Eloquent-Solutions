cc2af5b5b31f495b425754fe8cd07e140db0691b
Rishi Giri <rushforlinux@gmail.com>
2015-12-20 05:37:30 +0530

    Create recursion-puzzle-1.js

---
+++ 3/recursion-puzzle-1.js
@@ -0,0 +1,15 @@
+function find(start, history) {
+    if (start == target)
+      return history;
+    else if (start > target)
+      return null;
+    else
+      return find(start + 5, "(" + history + " + 5)") ||
+             find(start * 3, "(" + history + " * 3)");
+}
+
+function findSolution(target) {
+  return find(1, "1");
+}
+
+console.log(findSolution(24));
