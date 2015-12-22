90d9cfaf5b672980453755508b905ff73681d162
Rishi Giri <rushforlinux@gmail.com>
2015-12-22 22:06:39 +0530

    Create animCount.js

---
+++ 3/animCount.js
@@ -0,0 +1,15 @@
+function animFarm(cows, chickens) {
+	var cowString = String(cows);
+	while(cowString.length < 3) {
+		cowString = "0" + cowString;
+	}
+	console.log(cowString + " Cows");
+
+	var chickenString = String(chickens);
+	while(chickenString.length < 3) {
+		chickenString = "0" + chickenString;
+	}
+	console.log(chickenString + " Chickens");
+}
+
+animFarm(8,4)
