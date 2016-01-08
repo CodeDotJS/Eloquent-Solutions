5b8d339d0ed8c61bf42354bf6dc69134c418e771
Rishi Giri <rushforlinux@gmail.com>
2016-01-09 01:50:32 +0530

    Update tableFinder.js

--- 4/tableFinder.js
+++ 4/tableFinder.js
@@ -1,3 +1,12 @@
 function hasEvent(event, entry) {
    return entry.events.indexOf(event) != -1;
 }
+
+function tableFor(event, journal) {
+   var table = [0, 0, 0, 0];
+   for (var i = 0; i < journal.length; i++) {
+      var entry = journal[i],
+      var index = 0;
+      
+   }
+}
